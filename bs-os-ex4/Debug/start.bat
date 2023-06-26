84ca68cd735bc6c35798a81b63736de4f886f1a8
paoandrecm <paobloom@hotmail.com>
2023-06-26 19:23:19 +0200

    add .gitignore

--- bs-os-ex4/Debug/start.bat
+++
@@ -1,5 +0,0 @@
-start Control.exe
-timeout /T 0.5 /nobreak
-start Processing.exe
-timeout /T 0.5 /nobreak
-start InputOutput.exe
--- bs-os-ex4/x64/Release/start.bat
+++
@@ -1,5 +0,0 @@
-start Control.exe
-timeout /T 1 /nobreak
-start Processing.exe
-timeout /T 1 /nobreak
-start InputOutput.exe
